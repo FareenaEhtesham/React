a4c817ec30d42a763ffc9267c0bce7cb5ad75a9d
FareenaEhtesham <57524242+FareenaEhtesham@users.noreply.github.com>
2020-04-30 08:08:45 -0400

    Event Handler in React

--- basics/src/components/practice.js
+++ basics/src/components/practice.js
@@ -4,11 +4,12 @@
 //by Function
 //by Class
 import React from 'react'
+
 const practice = (props) =>{
 
     return(
         <div>
-            <h2>Hello my name is:{props.name} and i am {props.age} years old</h2>
+            <h2 onClick={props.click}>Hello my name is:{props.name} and i am {props.age} years old</h2>
             <p>{props.children}</p>
         </div>
     )
