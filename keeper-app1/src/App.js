36e33b3da83e8944c8839a33b7a6563ec4e41624
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-04 17:07:28 -0400

    main File of login/register

--- keeper-app1/src/App.js
+++ keeper-app1/src/App.js
@@ -1,36 +1,65 @@
 import React from 'react'
+import Header from './component/header'
+import Footer from './component/footer'
+import Note from './component/note'
+import Note_Card from './component/note_Card'
 import Card from './component/card'
 import Contact from './component/contact'
 import Avatar from './component/avatar'
 
 const App =() =>{
 
-let mapping;
+// let mapping;
 
-mapping =(
-  <div>
-        {Contact.map (a =>{
-        return(
-          <Card 
-          key = {a.id}      
-          name ={a.name}
-          img ={a.img}
-          phno={a.phno}
-          email={a.email}/>
-        )
+// mapping =(
+//   <div>
+//         {Contact.map (a =>{
+//         return(
+//           <Card 
+//           key = {a.id}      
+//           name ={a.name}
+//           img ={a.img}
+//           phno={a.phno}
+//           email={a.email}/>
+//         )
 
-        })}
-</div> 
-)
-    return(
-        <div>
-    <h1 className="heading">My Contacts</h1>
-    <Avatar image="https://media-exp1.licdn.com/dms/image/C5103AQHLO0kA5emSsw/profile-displayphoto-shrink_200_200/0?e=1596067200&v=beta&t=v2kr_Sq0Ya5yX4xUIm02NZyTlpLiILsrtohJWwTTCNA" />
+//         })}
+// </div> 
+// )
+//     return(
+//         <div>
+//     <h1 className="heading">My Contacts</h1>
+//     <Avatar image="https://media-exp1.licdn.com/dms/image/C5103AQHLO0kA5emSsw/profile-displayphoto-shrink_200_200/0?e=1596067200&v=beta&t=v2kr_Sq0Ya5yX4xUIm02NZyTlpLiILsrtohJWwTTCNA" />
     
-    {mapping}
+//     {mapping}
+    
 
-      </div>
-    )
+//       </div>
+//     )
+
+
+       return(
+         <div>
+
+           <Header />
+           
+           {Note.map( value =>{
+
+             return(
+              <Note_Card
+              key ={value.key}
+              title={value.title}
+              para = {value.content}
+              
+              />
+             )
+           }
+
+           )}
+           <Footer />
+           
+         </div>
+       ) 
      
 }
 
---
+++ keeper-app1/src/LogIn/login.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import Register from './component/register'
+
+var userIsRegistered = false
+
+// let islogin =true
+
+// const Logged =() =>{
+//     if(islogin === true)
+//     {
+//         return(
+//             <h1>Hello</h1>
+//         )
+//     }
+
+//     else{
+//         return(
+        
+//         <div>
+//                 {/* <Input type="text" pl="UserName"/>    
+//                 <Input type="password" pl="Password"/>     */}
+//                 <Form />
+//         </div>
+
+//         )
+           
+//     }
+// }
+
+
+const Login =() =>{
+
+    return(
+        <div className="container">
+                
+             {/* {islogin === true ? <Form/> : <h1>Hello</h1>}    */}
+
+             <Register isRegistered={userIsRegistered} />
+
+        </div>   
+    )
+
+}
+
+
+export default Login
