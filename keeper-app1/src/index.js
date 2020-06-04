0eaed850796ef47f756916a1e3ebe6f746ac1def
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-04 17:08:42 -0400

    Updated

--- keeper-app1/src/index.js
+++ keeper-app1/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Emoji from './Practice_Emoji'
+import Login from './LogIn/login'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
 
-    <Emoji />
+    {/* <Emoji /> */}
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
