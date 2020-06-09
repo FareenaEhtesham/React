139220b3561b8253fb393c2ba61ff176d04df441
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-09 02:08:28 -0400

    Updated

--- keeper-app1/src/index.js
+++ keeper-app1/src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import Emoji from './Practice_Emoji'
 import Login from './LogIn/login'
+import Decl_Imp from './Decl_Imp'
+import Time from './Time'
+import Main from './Hooks/main'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
 
-    <Emoji />
-    {/* <Login /> */}
+    {/* <Emoji /> */}
+    <Login />
+    {/* <Decl_Imp /> */}
+    {/* <Main/> */}
+	{/* <Time /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
