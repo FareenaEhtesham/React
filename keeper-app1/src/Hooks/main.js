83cc17c1fc25ca682cddfd20fb33aac50f992e2d
Fareena Ehtesham <shimmiehtesham229@gmail.com>
2020-06-30 05:53:18 -0400

    React modal component

---
+++ keeper-app1/src/Hooks/main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main =() =>{
+
+    return(
+       
+            <div className="container">
+                <h1>0</h1>
+                <button>+</button>
+            </div>
+     
+    )
+}
+
+
+export default Main
